1bcd38e37787dd4695f86334041f03d3b1c460bb
michalbenshalom <4154113@gmail.com>
2025-07-13 21:53:33 +0300

    commit

--- src/training_evaluation/Test/test_stage.py
+++ src/training_evaluation/Test/test_stage.py
@@ -6,7 +6,7 @@ from model_wrapper.models.base_model_wrapper import BaseModelWrapper
 from training_evaluation.Evaluation.evaluation import evaluate_model
 from src.config import MODEL_TYPE, CONFIG
 from utils.ModelsTypes import MODEL_WRAPPERS
-
+from torch.utils.data import Subset
 
 def test_model(model, test_dataset=None, weights_path=None,):
    
@@ -14,13 +14,14 @@ def test_model(model, test_dataset=None, weights_path=None,):
         raise ValueError(f"Unknown model type: {MODEL_TYPE}")
 
     # Load data
+    test_dataset = Subset(test_dataset, list(range(100))) #michalbs
     test_loader = DataLoader(test_dataset, batch_size=CONFIG["batch_size"], shuffle=False)
 
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
     
-    model_wrapper = ModelWrapperFactory.create(MODEL_TYPE, CONFIG)
+    model_wrapper = MODEL_WRAPPERS[MODEL_TYPE]() #ModelWrapperFactory.create(MODEL_TYPE, CONFIG)
     if not isinstance(model_wrapper, BaseModelWrapper):
         raise TypeError("ModelWrapper must inherit from BaseModelWrapper")
 
