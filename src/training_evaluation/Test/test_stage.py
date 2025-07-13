b67b9a913b23a07e13c4f080925be4d8a7984afb
michalbenshalom <4154113@gmail.com>
2025-07-13 20:23:42 +0300

    commit

--- src/training_evaluation/Test/test_stage.py
+++ src/training_evaluation/Test/test_stage.py
@@ -1,6 +1,8 @@
 
 import torch
 from torch.utils.data import DataLoader
+from model_wrapper.models.model_wrapper_factory  import ModelWrapperFactory
+from model_wrapper.models.base_model_wrapper import BaseModelWrapper
 from training_evaluation.Evaluation.evaluation import evaluate_model
 from src.config import MODEL_TYPE, CONFIG
 from utils.ModelsTypes import MODEL_WRAPPERS
@@ -18,8 +20,12 @@ def test_model(model, test_dataset=None, weights_path=None,):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
     
-    model_wrapper = MODEL_WRAPPERS[MODEL_TYPE]
-    model_wrapper.model = model
+    model_wrapper = ModelWrapperFactory.create(MODEL_TYPE, CONFIG)
+    if not isinstance(model_wrapper, BaseModelWrapper):
+        raise TypeError("ModelWrapper must inherit from BaseModelWrapper")
+
+    model_wrapper.set_model(model)
+
     # Evaluate
     eval_result = evaluate_model(model_wrapper, test_loader, device)
     return eval_result
