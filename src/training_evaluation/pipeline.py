0155f114f07d1d3ae47feccc6f252be037c2e4d4
infraedge <mirir@infraedge.co.il>
2025-07-15 10:21:03 +0300

    .

--- src/training_evaluation/pipeline.py
+++ src/training_evaluation/pipeline.py
@@ -4,7 +4,7 @@ from model_wrapper.data.image_utils import load_dataset
 from model_wrapper.data.dataset_loader import split_dataset
 from config import CONFIG, MODEL_TYPE, USE_EXISTING_MODEL
 from utils.ModelsTypes import MODEL_WRAPPERS
-from training_evaluation.train.trainer import trainfrom utils.s3_model_manager import S3ModelManager
+from training_evaluation.train.trainer import train from utils.s3_model_manager import S3ModelManager
 
 s3_manager = S3ModelManager()
 
