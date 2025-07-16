a84f75f216553319480dd113053dff4caf261456
michalbenshalom <4154113@gmail.com>
2025-07-16 22:16:11 +0300

    commit

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ from inference_pipeline.pipeline import predict_from_video_file
 from src.data_management.data_pipeline import process_and_validate_videos
 from training_evaluation.pipeline import run_models_pipeline
 from src.config import RELOAD_DATASET, MODEL_TYPE
-#from src.model_wrapper.train.test_only import test_model
 
 
 app = FastAPI()
@@ -40,16 +39,3 @@ async def predict_video(file: UploadFile = File(...)):
         import traceback
         print(traceback.format_exc())
         return {"error": str(e)}
-
-@app.get("/metrics")
-def metrics():
-    # weights = "resnet18_trained.pth" if MODEL_TYPE.lower() == "resnet" else "vit_b16_trained.pth"
-    # try:
-    #     result = test_model(model_type=MODEL_TYPE, weights_path=weights)
-    #     return {
-    #         "model": MODEL_TYPE,
-    #         "metrics": result
-    #     }
-    # except Exception as e:
-    #     return {"error": str(e)}
-    return {"message": "Metrics endpoint is not implemented yet."}
